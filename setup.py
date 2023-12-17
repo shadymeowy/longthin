ecb1a0f7e073080f6ab107d364aa97e9a9e512a5
shadymeowy <tolgademirdal@gmail.com>
2023-12-17 15:09:24 +0300

    add setup.py for proper packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='longthin',
+    version='0.0.1',
+    description='Long Thin Hauler',
+    author='Tolga Demirdal',
+    url='https://github.com/shadymeowy/longthin',
+    setup_requires=[],
+    install_requires=['drawing3d'],
+    packages=['longthin'],
+    entry_points={
+        'console_scripts': [
+            'longthin-sim = longthin.sim:main',
+            'longthin-test = longthin.test:main',
+        ],
+    },
+)
