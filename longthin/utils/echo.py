4cd5c3705a81f6a349f7bf702da173b58205ff6a
shadymeowy <tolgademirdal@gmail.com>
2024-02-17 00:50:25 +0300

    add option to filter packets in echo utility

--- longthin/utils/echo.py
+++ longthin/utils/echo.py
@@ -9,15 +9,22 @@ def main():
     parser = argparse.ArgumentParser(description='A packet echo client')
     parser.add_argument('--zmq', default=5555, help='ZMQ port')
     parser.add_argument('--zmq2', default=5556, help='ZMQ port2')
+    parser.add_argument('--filter', default=None, help='Filter packets', nargs='+')
     args = parser.parse_args()
     conn = LTZmq(args.zmq, args.zmq2, server=False)
+    if args.filter is not None:
+        typs = []
+        for name in args.filter:
+            typ = LTPACKET_TYPE[name.upper()]
+            typs.append(typ)
     while True:
         packet = conn.read()
         if packet is not None:
-            conn.send(packet)
+            if args.filter is not None and packet.type not in typs:
+                continue
             print(packet.type)
             print(yaml.dump(asdict(packet)), end='')
-        time.sleep(0)
+        time.sleep(1e-4)
 
 
 if __name__ == "__main__":
