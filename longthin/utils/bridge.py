b546312a27784483e264204e736f6017e8533e0f
shadymeowy <tolgademirdal@gmail.com>
2024-03-15 11:55:43 +0300

    allow bridge to run without any serial device

--- longthin/utils/bridge.py
+++ longthin/utils/bridge.py
@@ -7,16 +7,19 @@ from dataclasses import asdict
 
 def main():
     parser = argparse.ArgumentParser(description='Bridge between serial and zmq for ltpackets')
-    parser.add_argument('serial', help='Serial port')
+    parser.add_argument('serial', help='Serial port', default='', nargs='?')
     parser.add_argument('--zmq', default=5555, help='ZMQ port')
     parser.add_argument('--zmq2', default=5556, help='ZMQ port2')
     parser.add_argument('--baudrate', default=115200, help='Serial baudrate')
     parser.add_argument('--debug', action='store_true', help='Print debug messages')
     args = parser.parse_args()
-    ser = LTSerial(args.serial, baudrate=args.baudrate)
+    if args.serial:
+        ser = LTSerial(args.serial, baudrate=args.baudrate)
+    else:
+        ser = None
     conn = LTZmq(args.zmq, args.zmq2, server=True)
     while True:
-        while ser.is_available():
+        while ser and ser.is_available():
             packet = ser.read()
             conn.send(packet)
             if args.debug:
@@ -26,7 +29,8 @@ def main():
             packet = conn.read()
             if packet is None:
                 break
-            ser.write(packet)
+            if ser:
+                ser.write(packet)
             conn.send(packet)
             if args.debug:
                 print(packet.type, time.time())
