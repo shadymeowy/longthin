d08033c0359fd161b27c2e9c266fce82931c842a
shadymeowy <tolgademirdal@gmail.com>
2024-04-08 22:46:04 +0300

    fix sim for performance and apply proper scheduling

--- longthin/utils/sim.py
+++ longthin/utils/sim.py
@@ -3,6 +3,7 @@ import time
 import argparse
 from scipy.spatial.transform import Rotation as R
 
+from ..node import LTNode
 from ..graphics import LTRenderer
 from ..config import load_config
 from ..ltpacket import *
@@ -15,82 +16,93 @@ def main():
     parser.add_argument('--debug', action='store_true', help='Print debug messages')
     parser.add_argument('--video', default='shared:lt_video', help='Video sink')
     args = parser.parse_args()
-    
-    conn = LTZmq()
-    config = load_config()
-    renderer = LTRenderer(config)
-    width, height = config.camera.model.width, config.camera.model.height
+
+    node = LTNode()
+    config = node.config
+    params = node.params
+    renderer = LTRenderer(config, headless=True)
+    cam_model = config.camera.model
+    width, height = cam_model.width, cam_model.height
     if not args.video.startswith('shared:'):
         raise ValueError('Only shared memory video is supported')
     sink = args.video.strip('shared:')
     sink = SHMVideoWriter(sink, width, height)
-    params = default_params()
+
+    def cb_motor_output(packet):
+        nonlocal left, right
+        left, right = packet.left, packet.right
+
+    node.subscribe(MotorOutput, cb_motor_output)
 
     model = ddmr_dynamic_model(**config.sim.model._asdict())
-    dt = config.sim.dt
-    solver = RK4(model, 0, [0, 0, 0, 0, 0], dt)
+    sim_period = config.sim.dt
+    cam_period = config.sim.cam_dt
+    solver = RK4(model, 0, [0, 0, 0, 0, 0], sim_period)
     left, right = 0, 0
-    last_time = time.time()
     y = None
     vel_x_prev = 0
     vel_y_prev = 0
-    t_prev = time.time()
+    t_prev = time.perf_counter()
 
+    # I did not think knowing game loop strategies would be useful some day...
+    sim_accumulator = 0.0
+    cam_accumulator = 0.0
+    imu_accumulator = 0.0
+    spin_accumulator = 0.0
+    last_time = time.perf_counter()
+
+    y = solver.step((left, right))
     while True:
-        while True:
-            packet = conn.read()
-            if packet is None:
-                break
-            if isinstance(packet, Setparam):
-                params[LTParamType(packet.param)] = packet.value
-            elif isinstance(packet, Setparami):
-                params[LTParamType(packet.param)] = packet.value
-            elif isinstance(packet, Setparamu):
-                params[LTParamType(packet.param)] = packet.value
-            elif isinstance(packet, MotorOutput):
-                left, right = packet.left, packet.right
+        current_time = time.perf_counter()
+        delta_time = current_time - last_time
+        last_time = current_time
 
-        period = params[LTParamType.IMU_PUBLISH_PERIOD] / 1e6
-        if last_time + period < time.time():
-            img = renderer.render_image()
-            sink.write(img)
-            t = time.time()
-            step_count = (t - last_time) / dt
-            step_count = int(step_count) + 1
-            last_time += step_count * dt
-            sim_time = step_count * dt
-            calc_time = time.time()
-            for _ in range(step_count):
-                y = solver.step((left, right))
-            calc_time = time.time() - calc_time
-            rate = sim_time / calc_time
+        spin_accumulator += delta_time
+        sim_accumulator += delta_time
+        cam_accumulator += delta_time
+        imu_accumulator += delta_time
 
+        imu_period = params.imu_publish_period / 1e6
+        if imu_accumulator >= imu_period:
+            imu_accumulator -= imu_period
             rot = R.from_euler('xyz', [0, 0, y[2]], degrees=False)
             q = rot.as_quat()
-
             vel = y[1]
             vel_x = vel * np.cos(y[2])
             vel_y = vel * np.sin(y[2])
             dvx = vel_x - vel_x_prev
             dvy = vel_y - vel_y_prev
-            dvt = t - t_prev
+            dvt = current_time - t_prev
             vel_x_prev = vel_x
             vel_y_prev = vel_y
-            t_prev = t
+            t_prev = current_time
             dvz = 0
             packet = Imu(
                 q[3], q[0], q[1], q[2],
                 dvx, dvy, dvz,
                 dvt)
-            conn.send(packet)
-            packet = SimState(*y, rate)
-            conn.send(packet)
+            node.publish(packet)
+            packet = SimState(*y, 0)
+            node.publish(packet)
 
+        if sim_accumulator >= sim_period:
+            sim_accumulator -= sim_period
+            y = solver.step((left, right))
+
+        if cam_accumulator >= cam_period:
+            cam_accumulator -= cam_period
             renderer.vehicle_pose.pos = np.array([y[3], y[4], 0.])
             renderer.vehicle_pose.att = np.array([0., 0., np.rad2deg(y[2])])
+            renderer.update_coordinates()
+            img = renderer.render_image()
+            sink.write(img)
 
-        if renderer.draw():
-            break
+        spin_period = 1e-2
+        if spin_accumulator >= spin_period:
+            spin_accumulator -= spin_period
+            node.spin_once()
+
+        time.sleep(1e-3)
 
 
 if __name__ == "__main__":
