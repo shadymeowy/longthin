9cd2a3d88526aa166f11e64e481816013beccf70
shadymeowy <tolgademirdal@gmail.com>
2024-04-08 19:08:54 +0300

    switch to LTNode in longthin-reboot

--- longthin/utils/reboot.py
+++ longthin/utils/reboot.py
@@ -1,20 +1,18 @@
-from ..ltpacket import *
 import time
 import argparse
-import yaml
-from dataclasses import asdict
 
+from ..node import LTNode
+from ..ltpacket import *
 
 def main():
     parser = argparse.ArgumentParser(description='Reboot the microcontrollers')
-    parser.add_argument('--period', default=0.2, help='Blink period', type=float)
-    parser.add_argument('--debug', action='store_true', help='Print debug messages')
     args = parser.parse_args()
-    conn = LTZmq()
+
+    node = LTNode()
     reboot = Reboot(0)
-    while conn.read() is None:
-        time.sleep(1e-4)
-    conn.send(reboot)
+    while node.read() is None:
+        time.sleep(1e-2)
+    node.publish(reboot)
 
 
 if __name__ == "__main__":
