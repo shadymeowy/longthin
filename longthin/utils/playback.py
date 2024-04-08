7d2a5f468f8395d5615218420fc6b1220b9b2c09
shadymeowy <tolgademirdal@gmail.com>
2024-04-08 19:30:19 +0300

    switch LTNode in longthin-playback and optimize

--- longthin/utils/playback.py
+++ longthin/utils/playback.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import cv2
 
+from ..node import LTNode
 from ..ltpacket import *
 from ..config import load_config
 from ..shm import SHMVideoWriter
@@ -14,54 +15,52 @@ def main():
     parser.add_argument('--video', default='shared:lt_video', help='Video sink')
     args = parser.parse_args()
 
-    conn = LTZmq()
-    conf = load_config()
+    node = LTNode()
     file = LTFileReader(args.file + ".lt")
-    width, height = conf.camera.model.width, conf.camera.model.height
+    cam_model = node.config.camera.model
+    width, height = cam_model.width, cam_model.height
     if not args.video.startswith('shared:'):
         raise ValueError('Only shared memory video is supported')
     sink = args.video.strip('shared:')
     sink = SHMVideoWriter(sink, width, height)
 
     frames = []
-    for f in os.listdir(args.file):
-        t = float(os.path.splitext(f)[0])
-        f = os.path.join(args.file, f)
-        frames.append((t, f))
+    if os.path.isdir(args.file):
+        for f in os.listdir(args.file):
+            t = float(os.path.splitext(f)[0])
+            f = os.path.join(args.file, f)
+            frames.append((t, f))
     frames.sort(key=lambda x: -x[0])
 
     t_packet, packet = file.read()
-    t_frame, frame = frames.pop()
+    if len(frames) > 0:
+        t_frame, frame = frames.pop()
+    else:
+        t_frame, frame = t_packet, None
+
     offset_t = time.time() - min(t_packet, t_frame)
     while True:
-        min_sleep = 0
-        while True:
-            t = time.time() - offset_t
-            if t < t_packet or packet is None:
-                min_sleep = min(min_sleep, t_packet - t)
-                break
-            conn.send(packet)
+        t = time.time() - offset_t
+        remaining = 1e7
+        if t >= t_packet and packet is not None:
+            node.publish(packet)
             compound = file.read()
             if compound is None:
                 packet = None
-                break
-            t_packet, packet = compound
-        while True:
-            t = time.time() - offset_t
-            if t < t_frame or frame is None:
-                min_sleep = min(min_sleep, t_frame - t)
-                break
+            else:
+                t_packet, packet = compound
+                remaining = min(remaining, max(0, t_packet - t))
+        if t >= t_frame and frame is not None:
             img = cv2.imread(frame)
             sink.write(img)
             if len(frames) == 0:
                 frame = None
-                break
-            t_frame, frame = frames.pop()
+            else:
+                t_frame, frame = frames.pop()
+                remaining = min(remaining, max(0, t_frame - t))
         if packet is None and frame is None:
             break
-        if min_sleep > 0:
-            time.sleep(min_sleep * 0.5)
-
+        time.sleep(remaining)
     sink.close()
 
 
