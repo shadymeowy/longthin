fef8ac587b52351b59b6330d1b13fc6fc59ad231
shadymeowy <tolgademirdal@gmail.com>
2024-04-08 18:40:54 +0300

    switch to LTNode in longthin-echo and optimize it

--- longthin/utils/echo.py
+++ longthin/utils/echo.py
@@ -1,9 +1,11 @@
-from ..ltpacket import *
 import time
 import argparse
 import yaml
 import json
 from dataclasses import asdict
+
+from ..node import LTNode
+from ..ltpacket import *
 
 
 def main():
@@ -12,32 +14,32 @@ def main():
     parser.add_argument('--json', '-j', action='store_true', help='Print as json')
     parser.add_argument('--timestamp', '-t', action='store_true', help='Print timestamp')
     args = parser.parse_args()
-    
-    conn = LTZmq()
+
+    node = LTNode()
+
+    def callback(packet):
+        if args.json:
+            dct = {}
+            if args.timestamp:
+                dct['timestamp'] = time.time()
+            dct['type'] = str(packet.type)
+            dct.update(asdict(packet))
+            print(json.dumps(dct))
+        else:
+            print(packet.type)
+            if args.timestamp:
+                print(time.time())
+            print(yaml.dump(asdict(packet)), end='')
+
     if args.filter is not None:
-        typs = []
+        print('Filtering packets', args.filter)
         for name in args.filter:
             typ = LTPacketType[name.upper()]
-            typs.append(typ)
-    while True:
-        packet = conn.read()
-        if packet is not None:
-            if args.filter is not None and packet.type not in typs:
-                continue
-            if args.json:
-                dct = {}
-                if args.timestamp:
-                    dct['timestamp'] = time.time()
-                dct['type'] = str(packet.type)
-                dct.update(asdict(packet))
-                print(json.dumps(dct))
-            else:
-                print(packet.type)
-                if args.timestamp:
-                    print(time.time())
-                print(yaml.dump(asdict(packet)), end='')
-        else:
-            time.sleep(1e-4)
+            node.subscribe(typ.to_type(), callback)
+    else:
+        print('No filter specified')
+        node.subscribe(None, callback)
+    node.spin()
 
 
 if __name__ == "__main__":
