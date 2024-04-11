ef7355aa5ea90a868baf740e4945d72778d6d063
shadymeowy <tolgademirdal@gmail.com>
2024-04-12 00:59:48 +0300

    fix missing import

--- longthin/__init__.py
+++ longthin/__init__.py
@@ -8,6 +8,7 @@ from .abg import alpha_beta_filter
 from .node import LTNode
 from .rate import Rate
 from .park_detector import ParkDetector
+from .video_source import video_source
 try:
     from .raspicam import PiCam
 except ImportError:
