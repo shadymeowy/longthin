950b311f26d6de3eca0d065e04a2bbf836721078
shadymeowy <tolgademirdal@gmail.com>
2024-04-12 00:59:08 +0300

    update parking detector

--- longthin/park_detector.py
+++ longthin/park_detector.py
@@ -10,16 +10,24 @@ class ParkDetector:
             self,
             width,
             height,
+            inlier_threshold=100,
             height_offset=250,
+            width_offset=0,
             slope_limit=0.5,
+            hue_orange=(-5, 30),
+            hue_blue=(150, 270),
             chrome_orange=(0.2, 1.),
             intensity_orange=(0.15, 1.),
             chroma_blue=(0.05, 1.),
             intensity_blue=(0.01, 1.),
             storage=2048):
 
+        self.width = width
+        self.height = height
+        self.inlier_threshold = inlier_threshold
         self.height_offset = height_offset
         self.slope_limit = slope_limit
+        self.width_offset = width_offset
 
         self.mask_orange = np.zeros((height, width), dtype="uint8")
         self.mask_blue = np.zeros((height, width), dtype="uint8")
@@ -30,12 +38,10 @@ class ParkDetector:
         self.kernel1 = np.ones((5, 5), np.uint8)
         self.kernel2 = np.ones((25, 25), np.uint8)
 
-        hue_limit_orange = np.deg2rad(-15), np.deg2rad(+30)
-        hue_limit_blue = np.deg2rad(150), np.deg2rad(270)
         self.bounds_orange = calculate_lch_bounds(
-            hue_limit_orange, chrome_orange, intensity_orange)
+            hue_orange, chrome_orange, intensity_orange)
         self.bounds_blue = calculate_lch_bounds(
-            hue_limit_blue, chroma_blue, intensity_blue)
+            hue_blue, chroma_blue, intensity_blue)
 
     def process_image(self, image, debug=False):
         mask_image(image, self.bounds_blue, self.mask_blue)
@@ -65,11 +71,19 @@ class ParkDetector:
 
         k = self.slope_limit
         y1 = image.shape[0] - self.height_offset
+        inlier_count = 0
+        inliers_mins = None
         if ln_d > 100:
             for _ in range(4):
                 line = ransac_line(pps_d, inliers, count, delta, k)
                 inliers = inliers.astype(bool)
                 lines.append(line)
+                # TODO: Fix this
+                inliers_ps = pps_d[inliers[:pps_d.shape[0]]]
+                mn = np.min(inliers_ps[:, 1])
+                if inliers_mins is None or mn < inliers_mins:
+                    inliers_mins = mn
+                inlier_count += np.sum(inliers)
 
                 x = -(y1-line["py"])/line["nx"]*line["ny"]+line["px"]
                 intercepts.append(x)
@@ -84,11 +98,14 @@ class ParkDetector:
                 for x in intercepts:
                     cv2.circle(image, [int(x), int(y1)], 5, [255, 0, 0], -1)
                 cv2.circle(image, [int(mean_x), int(y1)], 5, [255, 0, 255], -1)
+                cv2.line(image, [int(image.shape[1] / 2), 0], [int(image.shape[1] / 2), image.shape[0]], [0, 0, 255], 2)
                 cv2.imshow("Result", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         else:
             mean_x = None
 
-        return mean_x
+        if inlier_count < self.inlier_threshold:
+            mean_x = None
+        return mean_x, inlier_count, inliers_mins
 
 
 def draw_line(image, px, py, nx, ny):
