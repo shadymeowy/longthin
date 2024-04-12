120b8452b7c9042eb6c574ffea0a21b3e55f2304
shadymeowy <tolgademirdal@gmail.com>
2024-04-12 12:53:54 +0300

    allow longthin-visualize to work on sim_state, ekf_state or ev_pose

--- longthin/utils/visualize.py
+++ longthin/utils/visualize.py
@@ -8,22 +8,25 @@ from ..ltpacket import *
 
 def main():
     parser = argparse.ArgumentParser(description='A visualization of the robot')
+    parser.add_argument('--mode', default='sim', choices=['sim', 'ekf', 'ev'], help='Mode of operation')
     args = parser.parse_args()
 
     node = LTNode()
     renderer = LTRenderer(node.config)
 
-    x, y, yaw = 0, 0, 0
-
-    def cb_evpose(packet):
-        nonlocal x, y, yaw
-        x = packet.x
-        y = packet.y
-        yaw = packet.yaw
+    def callback(packet):
+        x, y, yaw = packet.x, packet.y, packet.yaw
         renderer.vehicle_pose.pos = np.array([x, y, 0.])
         renderer.vehicle_pose.att = np.array([0., 0., yaw])
 
-    node.subscribe(EvPose, cb_evpose)
+    if args.mode == 'sim':
+        node.subscribe(SimState, callback)
+    elif args.mode == 'ekf':
+        node.subscribe(EkfState, callback)
+    elif args.mode == 'ev':
+        node.subscribe(EvPose, callback)
+    else:
+        raise ValueError('Invalid mode')
 
     while not renderer.draw():
         node.spin_once()
