e7f628af7315a2fdf29261df9602a56ef091bba5
shadymeowy <tolgademirdal@gmail.com>
2024-05-01 05:16:29 +0300

    add beeping while parking

--- longthin/controller/parking.py
+++ longthin/controller/parking.py
@@ -1,11 +1,15 @@
 from ..ltpacket import *
 from .vision import VisionController
+from ..notify import Notify
+import time
 
 
 class ParkingController(VisionController):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.node.subscribe(LaneVision, self.cb_lane_vision)
+        self.notify = Notify(self.node)
+        self.last_time = time.time()
 
     def setpoint(self):
         pass
@@ -14,8 +18,11 @@ class ParkingController(VisionController):
         mean_x = packet.mean_x
         min_y = packet.min_y
         self.mean_x = mean_x
-        # TODO: this needs to be parameterized
         if min_y is not None and min_y >= self.params.parking_visibility_limit:
             self.is_reached = True
         if min_y is not None and min_y < self.params.parking_visibility_limit:
             self.is_reached = False
+
+        if not self.enabled:
+            return
+        self.notify.beeping(1-min_y/self.params.parking_visibility_limit)
